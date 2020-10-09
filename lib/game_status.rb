bc556a4347cd1cd443b7241ec03af300011b7f66
carefree-window-8613 <jakobep24@gmail.com>
2020-10-09 14:53:09 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,6 +3,8 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+board = [" ", " ", " ", " ", " ", " ", " ", " "]
+
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
   [0, 1, 2],
@@ -14,3 +16,11 @@ WIN_COMBINATIONS = [
   [0, 4, 8],
   [2, 4, 6]
 ]
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |win_combo|
+    win_combo.all? do |win_index|
+      board[win_index] == "X" || board [win_index] == "O"
+    end
+  end
+end
