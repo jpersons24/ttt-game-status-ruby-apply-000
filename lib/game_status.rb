5a0bc43ebd7549bc6ce12afbccf4e30bcd129653
carefree-window-8613 <jakobep24@gmail.com>
2020-10-09 14:09:06 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,30 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+def won?(board)
+  if board == []
+    return false 
+  WIN_COMBINATIONS.find do |win_combo|
+    board[win_combo].each do |location|
+      if position_taken?(board, location)
+        location.all? do |symbol|
+          symbol == "X" || symbol =="O"
+          return win_combo
+        end
+      else
+        return false
+      end
+    end
+  end
+end
